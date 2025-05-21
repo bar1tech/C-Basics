49635aa2c48e3bf60b379792636b8c21f5900e90
bar1tech <52499915+bar1tech@users.noreply.github.com>
2025-05-20 20:27:52 -0400

    minor edits

--- chapter1/ex1-1/solution.c
+++ chapter1/ex1-1/solution.c
@@ -1,6 +1,19 @@
+/*
+ * #include <stdio.h>
+ * tells the compiler to include information about the standard input/output library;
+ * this line appears at the beginning of many C source files.
+ * The standard library is described in Chapter 7 and Appendix B
+ */
 #include <stdio.h>
 
-main()
+/* define a function named main that receives no argument value */
+int main()
 {
-    printf("hello, world\n");
-} 
+    /* main calls the library function printf to print this sequence of characters
+     * \n represents the newline character */
+    //printf("hello, world\n");
+    printf("\n");
+    printf("hello, ");
+    printf("world ");
+    printf("\n");
+}
--- chapter1/ex1-2/solution.c
+++ chapter1/ex1-2/solution.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-main()
+int main()
 {
-    printf("hello, world\n");    /* \n is valid */
+    printf("this, world\n");    /* \n is valid */
     printf("hello, world\c");    /* \c is invalid */
     printf("hello, world\7");    /* \7 is valid (bell) */
     printf("hello, world\?");    /* \? is valid */
